33d527c8e45a467292c02ef0cc0911f93bcf10d3
Simcha Greenbaum <gbs4ever2@gmail.com>
2020-01-20 01:19:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers,string) {
+
+return drivers.filter( n => {
+  return n.toUpperCase() === string.toUpperCase()
+} )
+
+}
+ function fuzzyMatch(drivers ,string){
+   return drivers.filter( n => {
+//let answer = n[0]
+     return n.toLowerCase().indexOf(string.toLowerCase())  === 0
+   } )
+
+ }
+function matchName (drivers, string){
+  return drivers.filter ( n=> n.name=== string )
+}
